b4608767d0ae4643b514183b8419038408cc322e
nagireddygatla <nagireddy.gatla@gmail.com>
2015-08-22 16:33:37 -0400

    Assignment Completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#Function is used to set and get input matrix/inverse
+#Similar to Bean class in java
+makeCacheMatrix <- function(z=matrix()){
+  
+  #function takes z matrix input
+  #returns a list with functions to
+  #set get input/inverse matrix
+  
+  invrs <- NULL
+  
+  #In this function we are setting input matrix using <<- operator
+  set <- function(p){
+    # <<- is a special operator to assign value and cache/store it
+    z <<- p
+    invrs <<- NULL
+    
+  }
+  #function to get input matrix
+  get<- function() z
+  #function to set inverse of matrix
+  setinverse <- function(inversed) invrs <<- inversed
+  #Function to get inverse of matrix
+  getinverse <- function() invrs
+  #return list
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+  
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#This function is used to get inverse of matrix if already set
+#If the inverse is null calculating it and setting it in makecachematrix
+cacheSolve <- function(z,...){
+  
+  #trying to get inverse matrix if its already set
+  invrs <- z$getinverse()
+  
+  #finding if fetched inverse matrix is null or not
+  #if not null returning
+  if(!is.null(invrs)){
+    
+    message("getting cached data")
+    return(invrs)
+    
+  }
+  
+  #finding the inverse of matrix
+  data <- z$get()
+  invrs <- solve(data,...)
+  
+  #setting the calculated inverse
+  z$setinverse(invrs)
+  return(invrs)
+  
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
